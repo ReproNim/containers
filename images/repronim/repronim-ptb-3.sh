75a325cb551f5c49173cd8c81a3739440c65160d
Yaroslav Halchenko <debian@onerussian.com>
2019-09-20 12:05:47 -0400

    A neurodocker invocation script to generate repronim-ptb-3 image

---
+++ images/repronim/repronim-ptb-3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO: ideally we should figure it out
+v=3.0.15.20190401.dfsg1-1~nd100
+v=$(echo $v | tr '~' '+')
+
+neurodocker generate singularity \
+   --base neurodebian:buster-non-free \
+   --ndfreeze date=20190915 \
+   --pkg-manager apt \
+   --install {octave,matlab}-psychtoolbox-3{,-nonfree} octave-{image,optim,signal,statistics} strace gdb valgrind \
+   >| Singularity.repronim-ptb-3--$v
