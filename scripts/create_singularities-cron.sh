6bed70bc0960e63f26a0227c6d77e50ca1a45147
Yaroslav Halchenko <debian@onerussian.com>
2022-07-13 17:03:56 -0400

    ENH: safeguard to ensure that datalad-container is avail before we run

--- scripts/create_singularities-cron.sh
+++ scripts/create_singularities-cron.sh
@@ -6,4 +6,10 @@ export PS4=+
 
 datalad wtf -S datalad -S dependencies -S extensions
 
+# verify that datalad-container is available in the environment
+if ! datalad containers-run --help >/dev/null 2>&1; then
+    echo "datalad-containers extension seems to be NA here"
+    exit 1
+fi
+
 $(dirname $0)/create_singularities
